80fbea14c939e8d035194b63ff7ffe8d33a7dbd0
ABDERRAHMAN ABOUMOUSLIM <abdourokinos@gmail.com>
2021-07-02 18:31:30 +0100

    myDate

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) { 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/blog.njk');
     eleventyConfig.addPassthroughCopy('./src/admin');
-     eleventyConfig.addFilter("postDate", (dateObj) => {
+    eleventyConfig.addFilter("myDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime)
     });
 
