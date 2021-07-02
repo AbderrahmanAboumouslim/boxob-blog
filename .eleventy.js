9c7fb96226763936d7a697b2b175cc82f00938fe
ABDERRAHMAN ABOUMOUSLIM <abdourokinos@gmail.com>
2021-07-02 18:00:59 +0100

    admin done

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/blog.njk');
-    eleventyConfig.addPassthroughCopy('./src/_includes/admin');
+    eleventyConfig.addPassthroughCopy('./src/admin');
+     eleventyConfig.addFilter("postDate", (dateObj) => {
+      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime)
+    });
 
   return {
     dir: {
