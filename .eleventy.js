d0fdb1938afa1f2f2aa7948077778eb2ce3e99af
ABDERRAHMAN ABOUMOUSLIM <abdourokinos@gmail.com>
2021-07-02 17:51:03 +0100

    admin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/blog.njk');
+    eleventyConfig.addPassthroughCopy('./src/_includes/admin');
 
   return {
     dir: {
